688097d8f9c420320849507d1608ebc32a281fa8
bakhuiwon326 <79272597+bakhuiwon326@users.noreply.github.com>
2022-03-03 00:30:18 +0900

    느슨한 연결
    Sort 인터페이스를 만들어 느슨한 연결을 하도록 함

--- src/main/java/com/java_oop/oop/Main.java
+++ src/main/java/com/java_oop/oop/Main.java
@@ -1,12 +1,13 @@
 package com.java_oop.oop;
 
 import com.java_oop.logic.BubbleSort;
+import com.java_oop.logic.Sort;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args){
-        BubbleSort<String> bubbleSort = new BubbleSort<>();
-        System.out.println("[result] " + bubbleSort.sort(Arrays.asList(args)));
+        Sort<String> sort = new BubbleSort<>();
+        System.out.println("[result] " + sort.sort(Arrays.asList(args)));
     }
 }
